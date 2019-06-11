fe0f9815a3aa1a6b7ecd286c9669fd3fbf3cb2aa
Unknown <paola.qdr@gmail.com>
2019-06-11 16:55:04 -0500

    Cambios REST API

--- REST/papeleria/models.py
+++ REST/papeleria/models.py
@@ -17,3 +17,25 @@ class Producto(models.Model):
 
     class Meta:
         ordering = ('categoria', 'nombre')
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    correo = models.EmailField()
+    contraseña = models.CharField(max_length=50)
+
+class Direccion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    direccion = models.CharField(max_length=255)
+    codigoPostal = models.IntegerField()
+    ciudad = models.CharField(max_length=50)
+    municipio = models.CharField(max_length=50)
+
+class Compra(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
+    subtotal = models.DecimalField(max_digits=6, decimal_places=2)
+    envio = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.IntegerField()
--- REST/papeleria/views.py
+++ REST/papeleria/views.py
@@ -1,11 +1,112 @@
-from .models import Producto
-from .serializer import ProductoSerializer
+from .models import Producto, Usuario, Direccion, Compra
+from .serializer import ProductoSerializer, UsuarioSerializer, DireccionSerializer, CompraSerializer
 from rest_framework  import generics
 
+#imports para class UsuarioList Y UsuarioDetail
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+
+
+#Clases para vistas Productos
+    #Método GET all
 class ProductoList(generics.ListAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
+    #Método GET único
 class ProductoDetail(generics.RetrieveAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+
+#Clases para vistas Usuarios
+class UsuarioList(APIView):
+    #Metodo GET all
+    def get(self, request, format=None):
+        usuarios = Usuario.objects.all()
+        serializer = UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data)
+    
+    #Metodo POST
+    def post(self, request. format=None):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            #Validar si usuario por registrar ya existe en la base de datos
+            existencia = Usuario.objects.filter(correo=serializer.validated_data['correo']).exists() 
+            banderaExist = 0
+        
+            #Si existe regresar una bandera con valor de 1
+            if existencia:
+                banderaExist = 1
+
+            #Si no existe regresar una bandera con valor de 0 y guardar el POST
+            else:
+                serializer.save()
+                banderaExist = 0
+
+            #Regresar valor banderaExist
+            return Response(banderaExist, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #Método GET único
+class UsuarioDetail(generics.RetrieveAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+
+
+#Clase para vistas Usuario Loign
+class UsuarioLogin(APIView):
+    #Metodo GET all
+    def get(self, request, format=None):
+        usuarios = Usuario.objects.all()
+        serializer = UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data)
+    
+    #Metodo POST
+    def post(self, request. format=None):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            #Validar si usuario por loguear ya existe en la base de datos
+            existencia = Usuario.objects.filter(correo=serializer.validated_data['correo']).exists() 
+            banderaExist = 0
+        
+            #Si existe regresar serializer con datos usuario
+            if existencia:
+                banderaExist = 1
+                usuario = Usuario.objects.get(correo=serializer.validated_data['correo'])
+                serializer = UsuarioSerializer(usuario)
+                return Response(serializer.data)
+
+            #Si no existe regresar una bandera con valor de 0 
+            else:
+                banderaExist = 0
+                return Response(banderaExist)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#Clases para vistas Direcciones
+    #Método GET all, POST
+class DireccionList(generics.ListCreateAPIView):
+    queryset = Direccion.objects.all()
+    serializer_class = DireccionSerializer
+
+    #Método GET único
+class DireccionDetail(generics.RetrieveAPIView):
+    queryset = Direccion.objects.all()
+    serializer_class = DireccionSerializer
+
+
+#Clases para vistas Compras
+    #Método GET all, POST
+class CompraList(generics.ListCreateAPIView):
+    queryset = Compra.objects.all()
+    serializer_class = DireccionSerializer
+
+    #Método GET único, UPDATE
+class CompraDetail(generics.RetrieveUpdateAPIView):
+    queryset = Compra.objects.all()
+    serializer_class = DireccionSerializer
